e26856175125cfbde00acfb26464b0413b43d772
Mortem <a20203607@pucp.edu.pe>
2023-08-24 21:59:02 -0500

    se agrego parcialmente que se muestre la imagenpucp si es que uso ?codigo=PUCP

--- lab1/src/main/java/com/example/lab1/controllers/Home.java
+++ lab1/src/main/java/com/example/lab1/controllers/Home.java
@@ -1,19 +1,28 @@
 package com.example.lab1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Home {
 
     @GetMapping(value = "")
-    public String listar() {
+    public String listar(Model model, @RequestParam(name = "code", required = false) String code) {
+
+        try{
+            if (code.equals("PUCP")){
+                model.addAttribute("ruta","img/logoPUCP");
+                System.out.println("si hubo code");
+            }
+        }catch (Exception e){
+            System.out.println("no hubo code");
+        }
+
+
         return "index";
     }
-
-
-
-
-
 
 }
